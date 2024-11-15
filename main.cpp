5843c584acf36474be6b5a628c10f8ed9afb6143
Linsay Itzep <sucelyicep@gmail.com>
2024-11-15 13:21:38 -0600

    Added Header

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
-#include <iostream>
+/**************************************************************************************
+Program:     Math Tutor
+Section:     4 - 1:00pm
+Programmers: Lindsay Itzep, Conner Diers
+Date:        10.30.2024
+Version:     4
+GitHub URL:  https://github.com/Linz215/Math_Tutor_V5.git
+Description: This program is an easy Math Tutor. It is a program that is intended to
+             help small children do simple math problems. This program will ask the
+             user to enter their name, and then ask them a math question until they
+             don't want more. The math program is able to tell if the input is a
+             number or not. It does all 4 math operations to help kids in all math
+             aspects. This program also levels up every three correct answers and
+             levels down every three wrong answers. After every question, the program
+             will ask the user if they want to continue. The program will only end when
+             the user says that they want to stop.
+**************************************************************************************/
+#include <iostream> //This allows the programmer the use of cin's and couts as well as
+#include <string>   //Needed for the user's name
+#include <ctime>    //Needed for the rand() commands to work
+#include <cstdlib>  //Used for the random number generator
+#include <limits>   //
+#include <vector>   //Sets up table for vectors
+#include <iomanip>  //Displays table
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
     return 0;
 }
